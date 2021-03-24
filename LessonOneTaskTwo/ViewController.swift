90025381192dde8422cbd73e1c4f1a0ae3224fd4
Philip4000 <6427676@bk.ru>
2021-03-24 17:18:19 +0300

    Final version of the second task. Smooth circles only on iPhone 12.

--- LessonOneTaskTwo/ViewController.swift
+++ LessonOneTaskTwo/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var redColorView: UIView!
+    @IBOutlet private weak var yellowColorView: UIView!
+    @IBOutlet private weak var greenColorVeiw: UIView!
+    @IBOutlet private weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 15
+        redColorView.alpha = 0.3
+        yellowColorView.alpha = 0.3
+        greenColorVeiw.alpha = 0.3
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        redColorView.layer.cornerRadius = 70
+        yellowColorView.layer.cornerRadius = 70
+        greenColorVeiw.layer.cornerRadius = 70
     }
 
-
+    @IBAction private func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redColorView.alpha == 0.3 {
+            redColorView.alpha = 1
+        } else if redColorView.alpha == 1 {
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 1
+        } else if yellowColorView.alpha == 1 {
+            yellowColorView.alpha = 0.3
+            greenColorVeiw.alpha = 1
+        } else {
+            greenColorVeiw.alpha = 0.3
+            redColorView.alpha = 1
+        }
+    }
+    
 }
 
